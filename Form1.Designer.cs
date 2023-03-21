8cbed1295a6885ad5e429045feab62c2db0aad46
Marredxxx <MariaRojas0306@umesoamericana.onmicrosoft.com>
2023-03-20 20:50:51 -0600

    Cambio en boton agregar

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,35 +28,243 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.buttonAgregarDatosVehiculo = new System.Windows.Forms.Button();
+            this.textBoxPlaca = new System.Windows.Forms.TextBox();
+            this.labelPlaca = new System.Windows.Forms.Label();
+            this.buttonAgregar = new System.Windows.Forms.Button();
+            this.labelPrecioxKilometro = new System.Windows.Forms.Label();
+            this.textBoxPrecioxKilometro = new System.Windows.Forms.TextBox();
+            this.textBoxModelo = new System.Windows.Forms.TextBox();
+            this.labelModelo = new System.Windows.Forms.Label();
+            this.textBoxMarca = new System.Windows.Forms.TextBox();
+            this.labelMarca = new System.Windows.Forms.Label();
+            this.labelColor = new System.Windows.Forms.Label();
+            this.textBoxColor = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dataGridView3 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
             this.SuspendLayout();
             // 
-            // buttonAgregarDatosVehiculo
+            // textBoxPlaca
             // 
-            this.buttonAgregarDatosVehiculo.Location = new System.Drawing.Point(297, 62);
-            this.buttonAgregarDatosVehiculo.Name = "buttonAgregarDatosVehiculo";
-            this.buttonAgregarDatosVehiculo.Size = new System.Drawing.Size(155, 70);
-            this.buttonAgregarDatosVehiculo.TabIndex = 0;
-            this.buttonAgregarDatosVehiculo.Text = "Agregar Datos del Vehiculo";
-            this.buttonAgregarDatosVehiculo.UseVisualStyleBackColor = true;
-            this.buttonAgregarDatosVehiculo.Click += new System.EventHandler(this.buttonAgregarDatosVehiculo_Click);
+            this.textBoxPlaca.Location = new System.Drawing.Point(330, 69);
+            this.textBoxPlaca.Name = "textBoxPlaca";
+            this.textBoxPlaca.Size = new System.Drawing.Size(129, 22);
+            this.textBoxPlaca.TabIndex = 24;
+            // 
+            // labelPlaca
+            // 
+            this.labelPlaca.AutoSize = true;
+            this.labelPlaca.Location = new System.Drawing.Point(327, 50);
+            this.labelPlaca.Name = "labelPlaca";
+            this.labelPlaca.Size = new System.Drawing.Size(42, 16);
+            this.labelPlaca.TabIndex = 23;
+            this.labelPlaca.Text = "Placa";
+            // 
+            // buttonAgregar
+            // 
+            this.buttonAgregar.Location = new System.Drawing.Point(330, 110);
+            this.buttonAgregar.Name = "buttonAgregar";
+            this.buttonAgregar.Size = new System.Drawing.Size(103, 34);
+            this.buttonAgregar.TabIndex = 22;
+            this.buttonAgregar.Text = "Agregar";
+            this.buttonAgregar.UseVisualStyleBackColor = true;
+            this.buttonAgregar.Click += new System.EventHandler(this.buttonAgregar_Click);
+            // 
+            // labelPrecioxKilometro
+            // 
+            this.labelPrecioxKilometro.AutoSize = true;
+            this.labelPrecioxKilometro.Location = new System.Drawing.Point(168, 103);
+            this.labelPrecioxKilometro.Name = "labelPrecioxKilometro";
+            this.labelPrecioxKilometro.Size = new System.Drawing.Size(127, 16);
+            this.labelPrecioxKilometro.TabIndex = 21;
+            this.labelPrecioxKilometro.Text = "Precio por kilómetro";
+            // 
+            // textBoxPrecioxKilometro
+            // 
+            this.textBoxPrecioxKilometro.Location = new System.Drawing.Point(166, 122);
+            this.textBoxPrecioxKilometro.Name = "textBoxPrecioxKilometro";
+            this.textBoxPrecioxKilometro.Size = new System.Drawing.Size(100, 22);
+            this.textBoxPrecioxKilometro.TabIndex = 20;
+            // 
+            // textBoxModelo
+            // 
+            this.textBoxModelo.Location = new System.Drawing.Point(166, 69);
+            this.textBoxModelo.Name = "textBoxModelo";
+            this.textBoxModelo.Size = new System.Drawing.Size(129, 22);
+            this.textBoxModelo.TabIndex = 19;
+            // 
+            // labelModelo
+            // 
+            this.labelModelo.AutoSize = true;
+            this.labelModelo.Location = new System.Drawing.Point(163, 50);
+            this.labelModelo.Name = "labelModelo";
+            this.labelModelo.Size = new System.Drawing.Size(53, 16);
+            this.labelModelo.TabIndex = 18;
+            this.labelModelo.Text = "Modelo";
+            // 
+            // textBoxMarca
+            // 
+            this.textBoxMarca.Location = new System.Drawing.Point(15, 122);
+            this.textBoxMarca.Name = "textBoxMarca";
+            this.textBoxMarca.Size = new System.Drawing.Size(129, 22);
+            this.textBoxMarca.TabIndex = 17;
+            // 
+            // labelMarca
+            // 
+            this.labelMarca.AutoSize = true;
+            this.labelMarca.Location = new System.Drawing.Point(12, 103);
+            this.labelMarca.Name = "labelMarca";
+            this.labelMarca.Size = new System.Drawing.Size(45, 16);
+            this.labelMarca.TabIndex = 16;
+            this.labelMarca.Text = "Marca";
+            // 
+            // labelColor
+            // 
+            this.labelColor.AutoSize = true;
+            this.labelColor.Location = new System.Drawing.Point(12, 50);
+            this.labelColor.Name = "labelColor";
+            this.labelColor.Size = new System.Drawing.Size(39, 16);
+            this.labelColor.TabIndex = 15;
+            this.labelColor.Text = "Color";
+            // 
+            // textBoxColor
+            // 
+            this.textBoxColor.Location = new System.Drawing.Point(16, 69);
+            this.textBoxColor.Name = "textBoxColor";
+            this.textBoxColor.Size = new System.Drawing.Size(129, 22);
+            this.textBoxColor.TabIndex = 14;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.label1.Location = new System.Drawing.Point(117, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(276, 16);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Por favor ingrese los datos que se le solicita: ";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(16, 210);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(536, 169);
+            this.dataGridView1.TabIndex = 25;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(594, 50);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowHeadersWidth = 51;
+            this.dataGridView2.RowTemplate.Height = 24;
+            this.dataGridView2.Size = new System.Drawing.Size(515, 143);
+            this.dataGridView2.TabIndex = 26;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.label2.Location = new System.Drawing.Point(591, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 16);
+            this.label2.TabIndex = 27;
+            this.label2.Text = "Clientes: ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.label3.Location = new System.Drawing.Point(13, 177);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(120, 16);
+            this.label3.TabIndex = 28;
+            this.label3.Text = "Datos del Vehículo";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.label4.Location = new System.Drawing.Point(591, 220);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(67, 16);
+            this.label4.TabIndex = 29;
+            this.label4.Text = "Alquileres";
+            // 
+            // dataGridView3
+            // 
+            this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView3.Location = new System.Drawing.Point(594, 250);
+            this.dataGridView3.Name = "dataGridView3";
+            this.dataGridView3.RowHeadersWidth = 51;
+            this.dataGridView3.RowTemplate.Height = 24;
+            this.dataGridView3.Size = new System.Drawing.Size(515, 150);
+            this.dataGridView3.TabIndex = 30;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.buttonAgregarDatosVehiculo);
+            this.ClientSize = new System.Drawing.Size(1147, 607);
+            this.Controls.Add(this.dataGridView3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.textBoxPlaca);
+            this.Controls.Add(this.labelPlaca);
+            this.Controls.Add(this.buttonAgregar);
+            this.Controls.Add(this.labelPrecioxKilometro);
+            this.Controls.Add(this.textBoxPrecioxKilometro);
+            this.Controls.Add(this.textBoxModelo);
+            this.Controls.Add(this.labelModelo);
+            this.Controls.Add(this.textBoxMarca);
+            this.Controls.Add(this.labelMarca);
+            this.Controls.Add(this.labelColor);
+            this.Controls.Add(this.textBoxColor);
+            this.Controls.Add(this.label1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Alquiler de Autos";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button buttonAgregarDatosVehiculo;
+        private System.Windows.Forms.TextBox textBoxPlaca;
+        private System.Windows.Forms.Label labelPlaca;
+        private System.Windows.Forms.Button buttonAgregar;
+        private System.Windows.Forms.Label labelPrecioxKilometro;
+        private System.Windows.Forms.TextBox textBoxPrecioxKilometro;
+        private System.Windows.Forms.TextBox textBoxModelo;
+        private System.Windows.Forms.Label labelModelo;
+        private System.Windows.Forms.TextBox textBoxMarca;
+        private System.Windows.Forms.Label labelMarca;
+        private System.Windows.Forms.Label labelColor;
+        private System.Windows.Forms.TextBox textBoxColor;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DataGridView dataGridView3;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,9 +14,9 @@ namespace Lab_6_Lab2_repaso___Empresa_de_autos
     public partial class Form1 : Form
     {
         List<Cliente> clientes = new List<Cliente>();
-        List<Vehiculo> vehiculos= new List<Vehiculo>();
-        List<Alquiler> alquileres= new List<Alquiler>();
-        List<Reporte> reportes= new List<Reporte>();//puede ir en el boton esta lista
+        List<Vehiculo> vehiculos = new List<Vehiculo>();
+        List<Alquiler> alquileres = new List<Alquiler>();
+        List<Reporte> reportes = new List<Reporte>();//puede ir en el boton esta lista
 
         public Form1()
         {
@@ -24,12 +25,34 @@ namespace Lab_6_Lab2_repaso___Empresa_de_autos
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            
         }
 
-        private void buttonAgregarDatosVehiculo_Click(object sender, EventArgs e)
+        private void buttonAgregar_Click(object sender, EventArgs e)
         {
+            bool localizado = false;
+            for (int i = 0; i < vehiculos.Count; i++)
+            {
+                if(vehiculos[i].Placa == textBoxPlaca.Text)
+                { 
+                    localizado = true;
+                }
+            }
+            Vehiculo pRepetida = vehiculos.Find(p => p.Placa == textBoxPlaca.Text);//lo buscara por la placa (find sirve para buscar a la persona)
+            
+            if (pRepetida == null)
+            {
+                Vehiculo vehiculo = new Vehiculo();//esto no se deja en global  para no tener problema al ingresar una segunda persona
+                vehiculo.Placa = textBoxPlaca.Text;
+                vehiculo.Marca = textBoxMarca.Text;
+                vehiculo.Modelo = Convert.ToInt32(textBoxModelo.Text);
+                vehiculo.Color = textBoxColor.Text;
+                vehiculo.PrecioKilometro = Convert.ToInt32(textBoxPrecioxKilometro.Text);
+                vehiculos.Add(vehiculo);
+            }
 
+            else
+                MessageBox.Show(pRepetida.Placa + " placa ya existente");
         }
     }
 }
